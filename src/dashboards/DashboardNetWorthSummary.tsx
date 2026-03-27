8b87171e48443e15e2f823b507df52625de07613
Roberto Rodriguez Alcala <rralcala@gmail.com>
2026-03-27 20:35:04 -0300

    Remove table head

--- src/dashboards/DashboardNetWorthSummary.tsx
+++ src/dashboards/DashboardNetWorthSummary.tsx
@@ -5,7 +5,6 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Stack } from '@mui/material';
@@ -43,13 +42,6 @@ export const DashboardNetWorthSummary = () => {
 
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Label</TableCell>
-                            <TableCell align="right">Value</TableCell>
-
-                        </TableRow>
-                    </TableHead>
                     <TableBody>
                         <TableRow
                             key={"capital"}
